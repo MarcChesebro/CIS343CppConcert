657a04cf00fe10f2c656b9b6487b6796e0e3d3ab
Marcellus Chesebro <chesebma@eos11.cis.gvsu.edu>
2017-10-09 20:38:36 -0400

    completed comments

--- Concert.cpp
+++ Concert.cpp
@@ -4,13 +4,20 @@
  * Author: Marc Chesebro
  *****************************************/
 
+//includes
 #include "Concert.h"
 #include <string>
 #include <vector>
 #include <iostream>
 #include <ctime>
 
-//constructors
+/*****************************************
+ *constructors
+ ****************************************/
+
+/*
+ * default Constructor
+ */
 Concert::Concert(){
 	this->concertName = "";
 	std::vector<std::string> friends;
@@ -23,6 +30,14 @@ Concert::Concert(){
 	this->date.tm_mday = 1;
 }
 
+/*
+ * Conctructor that takes all parameters
+ * concertName: std::string that holds the name of the concert
+ * friends: std::vector<strings> that holds the friends that
+ * 	you want to come with you
+ * desire: int for 1-10 that represents how much you want to go
+ * date: std::tm that hold the date of the concert
+ */
 Concert::Concert(std::string concertName, std::vector<std::string> friends, int desire, std::tm date){
 	this->concertName = concertName;
 	this->friends = friends;
@@ -30,24 +45,49 @@ Concert::Concert(std::string concertName, std::vector<std::string> friends, int 
 	this->date = date;
 }
 
-//getters
+/*****************************************
+ * getters
+ *****************************************/
+
+/*
+ *returns: concertName
+ */
 std::string Concert::getConcertName() const{
 	return concertName;
 }
 
+/*
+ *returns: friends in std::vector<std::string>
+ */
 std::vector<std::string> Concert::getFriends() const{
 	return friends;
 }
 
+/*
+ *returns: desire
+ */
 int Concert::getDesire() const{
 	return desire;
 }
 
+/*
+ *returns: date as std::tm
+ */
 std::tm Concert::getDate() const{
 	return date;
 }	
 
-//operator overides
+/*******************************
+ * operator overides
+ *******************************/
+
+/*
+ * Overriding the  < operator
+ * first by comparing the dates then by desire
+ *
+ * other: Concert& of the Concert to compare this too
+ * returns: true if this is less than other otherwise false
+ */
 bool Concert::operator<(const Concert& other) const{
 	
 	if(date.tm_year < other.getDate().tm_year){
@@ -66,6 +106,15 @@ bool Concert::operator<(const Concert& other) const{
 	return false;	
 }
 
+/*
+ * Overriding the << operator
+ *
+ * in format: <concertName> [mm/dd/yyyy] Desire: <desire>
+ *
+ * os: osstream to print to
+ * concert: concert to get information from
+ * returns: osstream full of data
+ */
 std::ostream& operator<<(std::ostream& os, const Concert& concert) {
 
 	os << concert.getConcertName() << " [" << concert.getDate().tm_mon + 1 << '/' << concert.getDate().tm_mday + 1 << '/' << concert.getDate().tm_year + 1900 << "] Desire: " << concert.getDesire();
